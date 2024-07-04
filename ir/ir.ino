4d4bd39692a61a379953cee5d4c93f51943e94aa
M1keNguy3n <mike.nguyen696@education.nsw.gov.au>
2024-07-05 00:26:47 +1000

    IR class (Completed)

--- ir/ir.ino
+++ ir/ir.ino
@@ -1,13 +1,15 @@
 #include "Arduino.h"
 #include "ir.h"
-#define IR_PIN A0
-IR r_ir(IR_PIN);
+#define IR_PIN_DIGITAL 5
+#define IR_PIN_ANALOG A0
+IR r_ir(IR_PIN_DIGITAL, IR_PIN_ANALOG);
 void setup() {
   // put your setup code here, to run once:
-  r_ir.init_digital();
+  r_ir.init();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   r_ir.readStateDigital();
+  r_ir.readStateAnalog();
 }
