fb7d9a8727926ae339671f44d6eeb432943e8e86
M1keNguy3n <mike.nguyen696@education.nsw.gov.au>
2024-07-05 09:58:23 +1000

    AI_Driver basic beheviours added

--- car/car.ino
+++ car/car.ino
@@ -11,16 +11,16 @@ Servo l_servo;
 Motor l_motor(l_servo);
 IR r_ir(R_IR_PIN_DIGITAL, R_IR_PIN_ANALOG);
 IR l_ir(L_IR_PIN_DIGITAL, L_IR_PIN_ANALOG);
+AI_Driver car(l_motor, r_motor, r_sensor, l_sensor);
 
 void setup() {
   // put your setup code here, to run once:
-  r_motor.attach(R_MOTOR_PIN);
-  l_motor.attach(L_MOTOR_PIN);
-  l_ir.init();
-  r_ir.init();
+  car.init();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  car.forward();
+  car.turn_left();
+  car.turn_right();
 }
