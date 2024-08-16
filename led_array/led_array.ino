14ac602cbc5dc6f3a5e34d0f53bb46880dfb7f3a
M1keNguy3n <mike.nguyen696@education.nsw.gov.au>
2024-08-16 15:03:12 +1000

    LED Matrix implemented.

--- led_array/led_array.ino
+++ led_array/led_array.ino
@@ -6,73 +6,21 @@
  * Tutorial page: https://arduinogetstarted.com/tutorials/arduino-uno-r4-led-matrix-displays-number-character
  */
 
-#include "Arduino_LED_Matrix.h"
-#include "fonts.h"
+#include "led_array.h"
+
 ArduinoLEDMatrix matrix;
-
-uint32_t frame[8][12] = {
-  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
-};
-
+LedMatrix LEDarray(matrix);
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200);
-  delay(1500);
-  matrix.begin();
-}
-
-
-void clear_frame() {
-  for (int row = 0; row < 8; row++) {
-    for (int col = 0; col < 12; col++) {
-      frame[row][col] = 0;
-    }
-  }
-}
-
-void display_frame() {
-  matrix.renderBitmap(frame, 8, 12);
-}
-
-
-void add_to_frame(char c, int pos) {
-  int index = 0;
-  if (c >= '0' && c <= '9')
-    index = c - '0';
-  else if (c >= 'A' && c <= 'Z')
-    index = c - 'A' + 10;
-  else {
-    Serial.println("WARNING: unsupported character");
-    return;
-  }
-
-  for (int row = 0; row < 8; row++) {
-    uint32_t temp = fonts[index][row] << (7 - pos);
-    for (int col = 0; col < 12; col++) {
-      frame[row][col] |= (temp >> (11 - col)) & 1;
-    }
-  }
+  LEDarray.begin();
 }
 
 void loop() {
-  clear_frame();
-  add_to_frame('0', 0);
-  add_to_frame('A', 6);
-  display_frame();
+  LEDarray.display_version();
   delay(1000);
 
-  clear_frame();
-  add_to_frame('0', 0);
-  add_to_frame('F', 6);
-  display_frame();
+  LEDarray.display_text('0');
   delay(1000);
 }
 
