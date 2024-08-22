2a9b61fe628b560c386f0800b5c4d37c3ecb8eae
Mike N <mike.nguyen696@education.nsw.gov.au>
2024-08-23 08:50:31 +1000

    Update led_array.cpp

--- car/led_array.cpp
+++ car/led_array.cpp
@@ -8,12 +8,12 @@ void LedMatrix::display_version(){
   //display the current version.
   matrix.beginText(0, 1, 0xFF, 0, 0);
   matrix.print("V00.00.01");
-  matrix.endText(SCROLL_LEFT);
+  matrix.endText();
 }
 
 void LedMatrix::display_text(char text){
   //display the given text.
   matrix.beginText(0, 1, 0xFF, 0, 0);
   matrix.print(text);
-  matrix.endText(SCROLL_LEFT);
+  matrix.endText();
 }
