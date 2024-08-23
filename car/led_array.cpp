3d5b565dbbc803c6389d663785720a01fc1238ea
Mike N <mike.nguyen696@education.nsw.gov.au>
2024-08-23 10:41:52 +1000

    Remove redundant code.

--- car/led_array.cpp
+++ car/led_array.cpp
@@ -6,20 +6,14 @@ LedMatrix::LedMatrix(ArduinoLEDMatrix &matrix){
 
 void LedMatrix::display_version(){
   //display the current version.
-  matrix.textFont(Font_5x7);
-  matrix.textScrollSpeed(100);
-  matrix.stroke(0xFF, 0, 0);
-  matrix.beginText(0, 1, 0xFF, 0, 0);
+  matrix.beginText();
   matrix.print("V00.00.01");
   matrix.endText();
 }
 
 void LedMatrix::display_text(char text){
   //display the given text.
-  matrix.textFont(Font_5x7);
-  matrix.textScrollSpeed(100);
-  matrix.stroke(0xFF, 0, 0);
-  matrix.beginText(0, 1, 0xFF, 0, 0);
+  matrix.beginText();
   matrix.print(text);
   matrix.endText();
 }
